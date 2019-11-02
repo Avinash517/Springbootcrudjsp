e707064ae2a74eedf420afb408450e56b3d8b84c
tsuser <tsuser@tsuser-Vostro-2520>
2019-11-02 17:48:40 +0530

    hello

---
+++ src/main/java/com/devcases/springboot/crud/library/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.devcases.springboot.crud.library.repository;
+
+import com.devcases.springboot.crud.library.model.Book;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+	@Query("SELECT b FROM Book b where b.name=?1")
+	public List<Book> getByName(String name);
+}
